41347aebd489520d179e602225d62b0f92d99874
Ugyen Choden <ugyen.choden@selise.ch>
2023-01-13 17:43:02 +0600

    Update app

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  include DeviseSanitizer
+
+  before_action :authenticate_user!
+  before_action :configure_permitted_params, if: :devise_controller?
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,11 @@ class User < ApplicationRecord
   include Devise::JWT::RevocationStrategies::JTIMatcher
 
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :timeoutable,
+         :recoverable, :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
+
+  has_one :profile, dependent: :destroy
+  belongs_to :role
+
+  accepts_nested_attributes_for :profile, allow_destroy: true
 end
