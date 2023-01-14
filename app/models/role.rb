49d8114da09999f63d6d8e967fcb7970b576c3c6
Ugyen Choden <ugyen.choden@selise.ch>
2023-01-15 02:45:38 +0600

    Fix: rubocop depreciation

--- app/models/role.rb
+++ app/models/role.rb
@@ -7,4 +7,6 @@ class Role < ApplicationRecord
   }
 
   has_many :users, dependent: :restrict_with_error
+
+  validates :name, presence: true, uniqueness: { case_sensitive: false }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,5 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'associations' do
+    it { is_expected.to belong_to(:role) }
+    it { is_expected.to have_one(:profile).dependent(:destroy) }
+  end
+
+  describe 'validations' do
+    it { is_expected.to validate_presence_of(:profile) }
+  end
 end
