2710dddd6949e78eb81b460c7e257259c52e1877
Ugyen Choden <ugyen.choden@selise.ch>
2023-01-15 04:10:44 +0600

    Initial graphql setup

--- app/graphql/chat_app_schema.rb
+++ app/graphql/chat_app_schema.rb
@@ -5,13 +5,12 @@ class ChatAppSchema < GraphQL::Schema
   query(Types::QueryType)
 
   # For batch-loading (see https://graphql-ruby.org/dataloader/overview.html)
-  use GraphQL::Dataloader
+  use GraphQL::Batch
 
   # GraphQL-Ruby calls this when something goes wrong while running a query:
 
   # Union and Interface Resolution
   def self.resolve_type(_abstract_type, _obj, _ctx)
-    # TODO: Implement this method
     # to return the correct GraphQL object type for `obj`
     raise(GraphQL::RequiredImplementationMissingError)
   end
--- app/graphql/types/base_object.rb
+++ app/graphql/types/base_object.rb
@@ -2,8 +2,18 @@
 
 module Types
   class BaseObject < GraphQL::Schema::Object
+    include GraphqlHelper
+
     edge_type_class(Types::BaseEdge)
     connection_type_class(Types::BaseConnection)
     field_class Types::BaseField
+
+    protected
+
+    def preload_association(association)
+      BatchLoaders::AssociationLoader
+        .for(object.class, association)
+        .load(object)
+    end
   end
 end
